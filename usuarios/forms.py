2c973fec79d251b7e653cf18f5b79148306b3c16
João Vitor de Oliveira <vitor-oliveira001@hotmail.com>
2024-12-03 14:47:10 -0300

    formularios, {% csrf_token %}, POST

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,11 +4,74 @@ class Formulario(forms.Form):
     nome_login = forms.CharField(
         label="Nome de Login", 
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder":"Ex: João Vitor"
+            }
+        )
     )
+
     senha = forms.CharField(
         label="Senha", 
         required=True,
         max_length=100,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder":"Digite sua senha"
+            }
+        
+        )
     )
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome de Cadastro", 
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder":"Ex: João Vitor"
+            }
+        )
+    )
+    email = forms.EmailField(
+        label="Email",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+        attrs={
+            "class": "form-control",
+            "placeholder": "Ex: joaosilva@email.com"
+        }
+
+        )
+    )
+
+    senha_1 = forms.CharField(
+       label="Senha", 
+       required=True,
+       max_length=100,
+       widget=forms.PasswordInput(
+           attrs={
+               "class": "form-control",
+               "placeholder":"Digite sua senha"
+           }
+       
+       )
+   )
+    senha_2 = forms.CharField(
+       label="Senha", 
+       required=True,
+       max_length=100,
+       widget=forms.PasswordInput(
+           attrs={
+               "class": "form-control",
+               "placeholder":"Digite sua senha novamente"
+           }
+       
+       )
+   )
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
-from usuarios.forms import Formulario
+from django.shortcuts import render, redirect
+from usuarios.forms import Formulario, CadastroForms
 
 def login(request):
     form = Formulario()
     return render(request, 'usuarios/login.html', {"form": form})
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    form = CadastroForms()
+
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+        if form['senha_1'].value() != form['senha_2'].value():
+            return redirect('cadastro')
+    return render(request, 'usuarios/cadastro.html', {"form": form})
+
