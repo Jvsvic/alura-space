d15feff816956dbf670566c4f076a6e4bee0aa06
João Vitor de Oliveira <vitor-oliveira001@hotmail.com>
2024-12-09 15:05:09 -0300

    return render

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -14,21 +14,25 @@ def imagem(request, foto_id):
         return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
 def buscar(request):
-        if not request.user.is_authenticated:
-                messages.error(request, 'Você precisa estar logado')
-                return redirect('login')
-        fotografias = Fotografia.objects.order_by("data").filter(publicada=True)
-        
-        if 'buscar' in request.GET:
-                nome_buscar = request.GET['buscar'].strip()
-                if nome_buscar:
-                        fotografias = fotografias.filter(nome__icontains=nome_buscar)
+    if not request.user.is_authenticated:
+        messages.error(request, 'Você precisa estar logado')
+        return redirect('login')
 
-        return render(request, 'galeria/buscar.html', {"cards": fotografias})
+    fotografias = Fotografia.objects.order_by("data").filter(publicada=True)
+    
+    if 'buscar' in request.GET:
+        nome_buscar = request.GET['buscar'].strip()
+        if nome_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_buscar)
 
-        def nova_imagem(request):
-                pass
-        def editar_imagem(request):
-                pass
-        def deletar_imagem(request):
-                pass
+    return render(request, 'galeria/buscar.html', {"cards": fotografias})  # Corrigido: fechando o parêntese
+
+
+def new_imagem(request):
+        return render(request, 'galeria/new_imagem.html')
+
+def editar_imagem(request):
+      pass
+
+def deletar_imagem(request):
+    pass        
